cd324f64610567b05dde69c95272b21e73392a77
PabloR03 <3014214630101@ingenieria.usac.edu.gt>
2023-03-01 21:36:52 -0600

    Esqueleto

---
+++ IPC1_S12023_PROYECTO1_202201947/src/main/java/IPC1_S12023_PROYECTO1_202201947.java
@@ -0,0 +1,14 @@
+/**
+ *
+ * @author Pablo Rodriguez
+ */
+public class IPC1_S12023_PROYECTO1_202201947 {
+
+    public static void main(String[] args) {
+        
+        LoginFrame loginframe = new LoginFrame();
+        loginframe.setVisible(true);
+        
+    }
+    
+}
